dd44f426324e29640bcf74ba10a593b8315911df
okki23 <okkisetyawan@gmail.com>
2022-11-15 14:39:37 +0700

    rubah datatablejs

--- assets/plugins/jquery-datatable/jquery.dataTables.js
+++ assets/plugins/jquery-datatable/jquery.dataTables.js
@@ -1733,7 +1733,7 @@
 		 * sZeroRecords - assuming that is given.
 		 */
 		if ( ! lang.sEmptyTable && zeroRecords &&
-			defaults.sEmptyTable === "No data available in table" )
+			defaults.sEmptyTable === "Data Tidak Tersedia atau Kosong" )
 		{
 			_fnMap( lang, lang, 'sZeroRecords', 'sEmptyTable' );
 		}
@@ -11349,7 +11349,7 @@
 			 * parameter - if it is not given, the value of `zeroRecords` will be used
 			 * instead (either the default or given value).
 			 *  @type string
-			 *  @default No data available in table
+			 *  @default Data Tidak Tersedia atau Kosong
 			 *
 			 *  @dtopt Language
 			 *  @name DataTable.defaults.language.emptyTable
@@ -11358,12 +11358,12 @@
 			 *    $(document).ready( function() {
 			 *      $('#example').dataTable( {
 			 *        "language": {
-			 *          "emptyTable": "No data available in table"
+			 *          "emptyTable": "Data Tidak Tersedia atau Kosong"
 			 *        }
 			 *      } );
 			 *    } );
 			 */
-			"sEmptyTable": "No data available in table",
+			"sEmptyTable": "Data Tidak Tersedia atau Kosong",
 	
 	
 			/**
